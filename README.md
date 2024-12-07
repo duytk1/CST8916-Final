c481a73488ca8f525217ec8b77d01022ade3a9c4
D <duytk.job@gmail.com>
2024-12-07 12:26:23 -0500

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
-s
+Step 1: create resource group
+
+![1.1](/images/1.1.png)
+
+Step 2: create IoT Hub
+
+Step 3: create device in IoT Hub
+
+Step 4: write python app and add connection string to code
+
+Step 5: create storage account
+
+Step 6: create containers
+
+Step 7: set up azure stream analytics
+
+Step 8: create input and output in stream analytics
+
+Step 9: write query and run it
+
+SELECT
+    name AS Name,
+    AVG(price) AS AvgStockPrice,
+    System.Timestamp AS EventTime
+INTO
+    [output]
+FROM
+    [input]
+WHERE
+    price > 30
+GROUP BY
+    name, TumblingWindow(second, 60)
+
+Step 10: update query with trade range, volume, market cap value
+
+SELECT
+    name AS Name,
+    price AS Price,
+    traderange AS TradeRange,
+    volume AS Volume,
+    market_Cap AS MarketCapValue,
+    System.Timestamp AS EventTime
+INTO
+    [output]
+FROM
+    IoTHubInput
+WHERE
+    price > 20
+GROUP BY
+    name, TumblingWindow(second, 60)
+
+
+Step 11: clean up resources
--- app.py
+++ app.py
@@ -1,33 +1,26 @@
 import time
 import random
 from azure.iot.device import IoTHubDeviceClient, Message
-from datetime import datetime
-import json
 
-def get_stock_data(symbol):
+def get_stock_data(name):
     return {
-        'symbol': symbol,
+        'name': name,
         'price': random.uniform(30.0, 100.0), 
         'timestamp': time.time(),
     }
-    
-def create_client(connection_string):
-    return IoTHubDeviceClient.create_from_connection_string(connection_string)
-    
+
 def send_message_azure(client, symbol):
     stock_data = get_stock_data(symbol)
-    event_data_batch = client.create_batch()
-    event_data_batch.add(Message(json.dumps(stock_data)))
-    client.send_batch(event_data_batch) 
+    message = Message(str(stock_data))
+    client.send_message(message)
     print(f'Sent stock data: {stock_data}')
     
 if __name__ == '__main__':
     print('Sending stock data to IoT Hub...')
-    # client = create_client('HostName=iothubforassignment.azure-devices.net;DeviceId=Device1;SharedAccessKey=urkO3O+R6tYV0yWCz7wbiDujmzrN28TvY45GjKa1WL8=')
-    client = IoTHubDeviceClient.create_from_connection_string("connection_string")
+    client = IoTHubDeviceClient.create_from_connection_string("HostName=iothubfinal.azure-devices.net;DeviceId=Device1;SharedAccessKey=n0ap3gt1E/xXFx8wkYj22CVdwfDPM2CsQI13pm4PgPA=")
     try:
         while True:
-            send_message_azure(client1, 'Dow\'s Lake')
+            send_message_azure(client, 'Algonquin College')
             time.sleep(10)
     except KeyboardInterrupt:
         print('Stopped sending messages.')
